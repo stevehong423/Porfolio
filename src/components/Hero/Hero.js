07c057143f55e2171818906b7a056bf43c461ac4
stevehong423 <stevehong423@hotmail.com>
2021-06-17 14:41:03 -0700

    first commit

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hey I'm, <br />
+        Steve Hong
+      </SectionTitle>
+      <SectionText>
+        I'm a full stack JavaScript software engineer, with a core focus on React/Redux architecture, Node.js, and SQL/NoSQL databases.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://linkedin.com/stevehong-swe'}>Learn More</Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
